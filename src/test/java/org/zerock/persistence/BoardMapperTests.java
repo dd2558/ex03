f773674f0c11450526b25326b4ddfc240bab8860
dd2558 <dd2558@naver.com>
2024-03-11 12:33:19 +0900

    BoardMapper추가

--- src/test/java/org/zerock/persistence/BoardMapperTests.java
+++ src/test/java/org/zerock/persistence/BoardMapperTests.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.mapper.BoardMapper;
-import org.zerock.mapper.TimeMapper;
 
 import lombok.extern.log4j.Log4j;
 
