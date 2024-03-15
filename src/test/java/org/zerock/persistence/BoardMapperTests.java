fe326484cc835d810561cdb71b434d8d058915c7
dd2558 <dd2558@naver.com>
2024-03-15 15:02:53 +0900

    paging test

--- src/test/java/org/zerock/persistence/BoardMapperTests.java
+++ src/test/java/org/zerock/persistence/BoardMapperTests.java
@@ -1,11 +1,14 @@
 package org.zerock.persistence;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.mapper.BoardMapper;
 
 import lombok.extern.log4j.Log4j;
@@ -65,6 +68,13 @@ public class BoardMapperTests {
 		
 		int count = mapper.update(board);
 		log.info("Update Count : " + count); // 성공시 1리턴 수정 실패시 0
+	}
+	
+	@Test
+	public void testPaging() {
+		Criteria cri = new Criteria();
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		list.forEach(board -> log.info(board));
 	}
 	
 	
